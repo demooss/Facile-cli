76eb248cc3959980bce3c3f54f5dcefc44809f02
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2022-09-01 17:49:20 +0900

    Add files via upload

--- packages/facile-cli/bin/facile.js
+++ packages/facile-cli/bin/facile.js
@@ -24,7 +24,17 @@
 
 const { program } = require('commander');
 const chalk = require('chalk');
+const fs = require('fs-extra');
+const path = require("path");
+const progress = require('cli-progress');
+const minimist = require('minimist');
+
 const packageJson = require('../package.json');
+const bar = new progress.SingleBar({}, {
+    format: '[{bar}]({value}/{total}) {percentage}% | {eta}s',
+    barCompleteChar: '=',
+    barIncompleteChar: ' '
+});
 
 program
     .version(packageJson.version, '-v, --version')
@@ -37,23 +47,33 @@ program
     .option('-d, --directory [path]','Specifies the path.','./src')
     .option('-p, --port [port]','Specifies the port.','8080')
     .action((option)=>{
-        console.log(option.directory);
-        console.log(option.port);
+        
     });
 
 program
     .command('create <name>')
     .usage('<name>')
     .description('Create a new project.')
-    .action((name)=>{
-        console.log(name);
+    .action((projectName)=>{
+        if (minimist(process.argv.slice(3))._.length > 1) {
+            console.log(chalk.yellow('\n Info: You provided more than one argument. The first one will be used as the app\'s name, the rest are ignored.'))
+        }
+    
+        console.clear();
+        console.log(chalk.bold.yellow('< Facile.js CLI >'));
+        bar.start(2, 0);
+        bar.update(1);
+        bar.stop();
+        const cwd = process.cwd();
+        const inCurrent = projectName === '.';
+        const name = inCurrent ? path.relative('../', cwd) : projectName;
+        const targetDir = path.resolve(cwd, projectName || '.');
     });
-
 
 program
     .action((cmd, args)=>{
         if(args){
-            console.log(chalk.bold.red('The command could not be found.'));
+            console.log(chalk.bold.red('Error: The command could not be found.'));
         }
     })
     .parse(process.argv);
